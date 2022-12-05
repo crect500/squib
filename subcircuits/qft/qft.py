ed18f675e9786474846a2c7c5c45cf03ba90eaf3
crect500 <chrisrector14@gmail.com>
2022-12-04 18:01:57 -0700

    Remove main function from qft module

--- subcircuits/qft/qft.py
+++ subcircuits/qft/qft.py
@@ -2,20 +2,17 @@ from qiskit import QuantumCircuit,  \
                     QuantumRegister, \
                     ClassicalRegister
 from qiskit.circuit.library import HGate
-from math import log2, pi
+from math import pi
 
 
 def qft(qc: QuantumCircuit) -> QuantumCircuit:
     n = qc.num_qubits
+    if n < 1:
+        raise ValueError("The circuit must have at least one qubit")
+
     for i in range(0, n):
         qc.h(i)
         for j in range(i + 1, n):
             qc.cp(pi/(2**(j - i)), j, i)
 
     return qc
-        
-
-if __name__ == "__main__":
-    circuit = QuantumCircuit(4)
-    circuit = qft(circuit)
-    print(circuit)
